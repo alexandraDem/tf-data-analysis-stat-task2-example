87bc23160083556a079d9d89504f397af7f9a9ce
Alexandra <55894317+alexandraDem@users.noreply.github.com>
2023-04-01 11:30:57 +0300

    Attempt 1

--- solution.py
+++ solution.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import norm
+from scipy.stats import chi2
 
 
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
@@ -13,5 +14,14 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     loc = x.mean()
     scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    
+    n = len(x)
+    s2=np.var(x, ddof=1)
+    q1 = chi2.ppf(p/2, df = n -1)
+    q2 = chi2.ppf(alpha/2, df = n-1)
+    left = np.sqrt((n-1)*s2/q2)/np.sqrt(48)
+    right =  np.sqrt((n-1)*s2/q1)/np.sqrt(48)
+    
+    return (left, right)
+
+
