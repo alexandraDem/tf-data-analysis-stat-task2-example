322c5536d753d5c446cb6eeeba0a530f91ac7e34
Alexandra <55894317+alexandraDem@users.noreply.github.com>
2023-04-06 16:43:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,16 +12,16 @@ def solution(p: float, x: np.array) -> tuple:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    
     n = len(x)
-    s2=np.var(x, ddof=1)
-    q1 = chi2.ppf(p/2, df = n -1)
-    q2 = chi2.ppf(alpha/2, df = n-1)
-    left = np.sqrt((n-1)*s2/q2)/np.sqrt(48)
-    right =  np.sqrt((n-1)*s2/q1)/np.sqrt(48)
     
-    return (left, right)
+    x2 = np.array([xi**2 for xi in x])
+    x2_mean = x2.mean()
+    
+    chi2_rv = chi2(df = 2 * n)
+    
+    left = chi2_rv.ppf(1 - alpha / 2) #b
+    right = chi2_rv.ppf(alpha / 2) #a
+    
+    return np.sqrt(n * x2_mean / (left * 48)), np.sqrt(n * x2_mean / (right * 48))
 
 
